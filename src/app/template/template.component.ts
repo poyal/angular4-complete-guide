762046a1e3f3ee15a7f5480065067984069dfb3a
poyal <ttt4504@gmail.com>
2017-07-20 13:39:52 +0900

    component selector

--- src/app/template/template.component.ts
+++ src/app/template/template.component.ts
@@ -1,10 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-template',
+  selector: '[app-template]',
   template: `
     <app-server></app-server>
-    <app-servers></app-servers>`,
+    <app-server></app-server>`,
   styleUrls: ['./template.component.css']
 })
 export class TemplateComponent implements OnInit {
